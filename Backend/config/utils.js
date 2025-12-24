ab447e465188c20f15e408dc8ce9b60f5ac605d6
Mohand hatem <146125801+Mohand-Hatem@users.noreply.github.com>
2025-12-24 16:08:45 +0200

    Update utils.js

--- Backend/config/utils.js
+++ Backend/config/utils.js
@@ -14,7 +14,7 @@ export const generateToken = (newUser, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: false,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
