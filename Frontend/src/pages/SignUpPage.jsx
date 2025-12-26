357d509f21ffe7840299e23e148fa0989fdd5282
Mohand hatem <146125801+Mohand-Hatem@users.noreply.github.com>
2025-12-26 18:19:42 +0200

    Update SignUpPage.jsx

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -27,7 +27,7 @@ function SignUpPage() {
       .min(8, "Password must be at least 8 characters")
       .regex(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        "Password must contain at least one uppercase letter, one number, one special character"
+        "Password must be Strong include (uppercase, number, special character)"
       ),
   });
   const {
